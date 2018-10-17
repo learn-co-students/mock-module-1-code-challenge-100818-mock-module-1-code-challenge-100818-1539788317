d7a53c9d52b90a8666f91808a50ae13e5203573e
AHCarl <ahcarlin@gmail.com>
2018-10-17 10:46:39 -0500

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,37 @@
+class Author 
+  attr_accessor :name, :books
+  @@all = []
 
+  def initialize(name)
+    self.name = name 
+    self.books = []
+    self.class.all << self
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def write_book(title, word_count)
+    new_book = Book.new(title, word_count, self)
+    new_book.title = title 
+    new_book.word_count = word_count
+    self.books << new_book
+  end
+
+  def total_words
+    total = 0
+    self.books.each do |book|
+      total += book.word_count
+    end
+    total
+  end
+
+  def self.most_words
+    max_auth = self.all.max_by {|auth| auth.total_words}
+    max_auth.name
+  end
+  
+
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,19 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+tommy = Author.new("tommy")
+billy = Author.new("billy")
+
+tommy.write_book("figaro", 123)
+tommy.write_book("carmen", 321)
+billy.write_book("opera", 1222)
+castle = Book.new("castles", 242, billy)
+
+
+tommy.total_words
+Author.most_words
+
+
 binding.pry
 
 
