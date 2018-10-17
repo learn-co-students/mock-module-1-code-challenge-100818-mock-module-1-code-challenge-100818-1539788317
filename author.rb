8ac21b7cea4c2e6eab2d734a6f6ad8a9e8cfd31b
Megan Farley <megfarley4@gmail.com>
2018-10-17 11:10:55 -0500

    megan mock done

--- author.rb
+++ author.rb
@@ -1,1 +1,49 @@
+require 'pry'
+require_relative './book'
 
+class Author
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        self.name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def write_book(title, word_count)
+        Book.new(self, title, word_count)
+    end
+
+    def total_words
+        #return the total number of words 
+        #that author has written across all of their authored books.
+        author_book = Book.all.select do |book|
+            book.author == self
+        end
+        sum = 0
+        author_book.each do |book|
+            sum += book.word_count
+        end
+        sum
+    end
+
+    def self.most_words
+        # return the author instance who has written the most words
+        words = 0
+        author_most = nil
+        self.all.each do |author|
+            if author.total_words > words
+                words = author.total_words
+                author_most = author
+            end
+        end
+        author_most
+    end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,23 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+# book1 = Book.new("JK R", "Harry Potter 1", 200)
+# book2 = Book.new("JK R", "Harry Potter 2", 300)
+# book3 = Book.new("JRR T", "LOTR", 200)
+
+author1 = Author.new("JK R")
+author2 = Author.new("JRR T")
+
+book1 = author1.write_book("Harry Potter 1", 200)
+book2 = author1.write_book("Harry Potter 2", 300)
+book3 = author2.write_book("LOTR", 200)
+
+#binding.pry
+author1_tot_words = author1.total_words
+author2_tot_words = author2.total_words
+
+author_most_words = Author.most_words
+
 binding.pry
 
 
