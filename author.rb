24b13e11e7f3772beaf67195b4a30a16f820d1fc
github username <github email address>
2018-10-17 10:40:26 -0500

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,43 @@
+class Author
+    attr_accessor :name, :title_array
+    @@all = []
 
+    def initialize(name)
+        self.name = name
+        @@all << self
+        self.title_array = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        self.title_array
+    end
+
+    def write_book(title, word_count)
+        self.title_array << Book.new(title, word_count)
+    end
+
+    def total_words
+        result = 0
+        self.books.each do | book |
+            result += book.word_count
+        end
+        result
+    end
+
+    def self.most_words
+        highest_word_count = 0
+        prolific_author = nil
+
+        Author.all.each do | author |
+            if author.total_words > highest_word_count
+                prolific_author = author
+                highest_word_count = author.total_words
+            end
+        end
+        prolific_author
+    end
+end 
