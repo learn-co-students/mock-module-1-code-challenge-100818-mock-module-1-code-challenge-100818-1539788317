53d07e944d0f651356fa97af458cd1e9acaf481f
jordan-laird <jordanmack.laird@gmail.com>
2018-10-17 11:48:15 -0500

    jordan

--- console.rb
+++ console.rb
@@ -2,6 +2,14 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+jordan = Author.new("Jordan")
+amber = Author.new("Amber")
+
+ex1 = Book.new("book title", 3132, jordan)
+ex2 = Book.new("book2", 343, amber)
+ex3 = Book.new("book3", 343, amber)
+
+
 binding.pry
 
 
