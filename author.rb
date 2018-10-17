c02ecacbe2ce62ef7aecc236819c86fbe1fe37bc
github username <github email address>
2018-10-17 11:20:33 -0500

    kida done but not really

--- author.rb
+++ author.rb
@@ -1,1 +1,39 @@
+class Author
+    
+    @@ll = []
 
+    attr_accessor  :book, :title
+    
+
+    def initialize(book, title)
+        self.book = book
+        self.title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_books
+        author.all each do |books|
+            @@all < books
+        end
+    end
+
+    def write_book(title, word_count)
+        author.new("Books Are Lame", 99)
+    end
+
+    def total_words
+        books.each do |words|
+            book.word_count.split.size
+            end
+    end
+
+    def most_words
+        word_count.count
+    end
+
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,34 @@
 
+require 'pry'
+
+class Book
+
+    @@ll = []
+
+    attr_accessor   :name, :title
+    attr_reader :author
+
+    def initialize(author, title)
+        self.author = author
+        self.title = title
+        @@all < self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author
+        @author
+    end
+
+    def title
+        @title
+    end
+
+    def word_count
+        books.each do |book|
+        book.word_count.split.size
+        end
+    end
+end
