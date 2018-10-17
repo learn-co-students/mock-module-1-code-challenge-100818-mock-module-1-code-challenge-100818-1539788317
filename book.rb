4c6308241776bac4d3c5cc8bec6d6c6bb82393bb
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-17 11:07:39 -0500

    Done

--- book.rb
+++ book.rb
@@ -1,1 +1,28 @@
+class Book
+    attr_accessor :author, :title
+    @@all = []
+    
+    # def initialize
+    #     @author = author
+    #     @title = title
+    #     @@all << self
+    # end
 
+    def self.all
+        @@all
+    end
+
+    def author
+        Author.all.select do | author |
+            author.book == self
+    end
+
+    def word_count
+        number_of_words > 0
+        self.author.each do | word |
+            if number_of_words = word_count.book
+               
+            end
+    end
+    
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,11 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+book = Book.new('book')
+book.author = self
+
+
 binding.pry
+
 
 
